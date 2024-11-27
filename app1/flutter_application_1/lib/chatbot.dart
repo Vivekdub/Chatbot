9a2915dc090b907929a3c7058399f8910db1ece5
Vivek Dubey <126369758+Vivekdub@users.noreply.github.com>
2024-11-28 00:58:42 +0530

    Update chatbot.dart

--- app1/flutter_application_1/lib/chatbot.dart
+++ app1/flutter_application_1/lib/chatbot.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show Platform;
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
